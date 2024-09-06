3cfaccebc96899a330a6da646d729fd446dca908
Hippolyte Lévêque <hippolyte.leveque@essec.edu>
2024-09-06 14:25:19 +0200

    Made environment config possible for setting up mongohost

--- 1-data-crawling/config.py
+++ 1-data-crawling/config.py
@@ -1,10 +1,12 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file="../.env", env_file_encoding="utf-8")
 
-    MONGO_DATABASE_HOST: str = (
+    MONGO_DATABASE_HOST: str = os.getenv(
+        "MONGO_DATABASE_HOST",
         "mongodb://mongo1:30001,mongo2:30002,mongo3:30003/?replicaSet=my-replica-set"
     )
     MONGO_DATABASE_NAME: str = "scrabble"
